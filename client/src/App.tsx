ce43bbafd1dc76716d77ba854a0454397afa9e12
PETER ABBOTT <abbotsayshi@gmail.com>
2024-01-18 15:03:57 +0000

    added input

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
 function App() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <>
       <div>
         <h1>Welcome!</h1>
+        <form onSubmit={handleSubmit}>
+          <label>search</label>
+          <input />
+          <button>Submit query</button>
+        </form>
       </div>
     </>
   );
