c6bf870fff696d2b8624c40cdb899e2fde9c733c
PETER ABBOTT <abbotsayshi@gmail.com>
2024-01-18 16:20:08 +0000

    added form logic to get form value and console.log

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,36 @@
 function App() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  // handles form
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    const formElement = document.getElementById(
+      "searchForm"
+    ) as HTMLFormElement;
+    const formData = new FormData(formElement);
+
+    const searchValue = formData.get("searchInput");
+
+    console.log(searchValue);
+
+    fetch("/agg_req", {
+      method: "GET",
+      body: formData,
+    });
   };
 
   return (
     <>
       <div>
         <h1>Welcome!</h1>
-        <form onSubmit={handleSubmit}>
+        <form id="searchForm" onSubmit={handleFormSubmit}>
           <label>search</label>
-          <input />
+          <input type="text" name="searchInput" placeholder="Search" />
           <button>Submit query</button>
         </form>
       </div>
+      <section>
+        <h3>Search results:</h3>
+        <div>{/* //results go here */}</div>
+      </section>
     </>
   );
 }
